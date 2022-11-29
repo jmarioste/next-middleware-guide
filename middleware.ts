d561b37669551d74d1ff4317ad3f5bb887ba97b8
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-11-29 11:42:29 +0800

    Add authorization

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { stackMiddlewares } from "middlewares/stackMiddlewares";
+import { withAuthorization } from "middlewares/withAuthorization";
 import { withHeaders } from "middlewares/withHeaders";
 import { withLogging } from "middlewares/withLogging";
 
-export default stackMiddlewares([withLogging, withHeaders]);
+export default stackMiddlewares([withAuthorization, withLogging, withHeaders]);
